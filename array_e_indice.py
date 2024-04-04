0daa8a440de00ac7a14fe4d007599b2568d2968e
victor samuel oliveira <victorsamueloliveira59@gmail.com>
2024-04-04 20:36:29 -0300

    Resolve questão 03

--- array_e_indice.py
+++ array_e_indice.py
@@ -14,3 +14,25 @@ def imprimi_ultimo_nome():
     print("Ultimo nome:", nomes[3])
 
 imprimi_ultimo_nome()
+
+def troca_nome_lista():
+
+    lista_mercado = ["Macarrão", "Pepino", "Batata"]
+
+    print("Está é a sua lista atual:", lista_mercado)
+    print("Digite o nome do item que deseja trocar:")
+
+    novo_item1 = str(input("1: "))
+    novo_item2 = str(input("2: "))
+    novo_item3 = str(input("3: "))
+
+    lista_mercado[0] = novo_item1
+    lista_mercado[1] = novo_item2
+    lista_mercado[2] = novo_item3
+
+    print("Nova lista do mercado:")
+    print("1 -", lista_mercado[0])
+    print("2 -", lista_mercado[1])
+    print("3 -", lista_mercado[2])
+
+troca_nome_lista()
