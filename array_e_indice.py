7f5b38ce6aeaa62e403d6c51b9771fc0609f7588
victor samuel oliveira <victorsamueloliveira59@gmail.com>
2024-04-04 20:21:02 -0300

    Resolve questão 01

---
+++ array_e_indice.py
@@ -0,0 +1,9 @@
+def imprimi_nomes():
+    nomes = ['Joao', 'Maria', 'Fulano', 'Siclano']
+
+    print("1 -", nomes[0])
+    print("2 -", nomes[1])
+    print("3 -", nomes[2])
+    print("4 -", nomes[3])
+
+imprimi_nomes()
