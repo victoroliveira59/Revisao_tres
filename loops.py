d15a8f9011c146e039dd74dd070c4054144542c2
victor samuel oliveira <victorsamueloliveira59@gmail.com>
2024-04-04 21:36:46 -0300

    Update questao

--- loops.py
+++ loops.py
@@ -1,15 +1,15 @@
-# def contar_ate_vinte():
-#     for i in range(20):
-#         print(i)
-#
-# contar_ate_vinte()
-#
-# def conte_usuario():
-#     contagem_usuario = int(input("Digite um número para poder fazer a contagem: "))
-#
-#     for i in range(1, contagem_usuario):
-#         print(i)
-# conte_usuario()
+def contar_ate_vinte():
+    for i in range(20):
+        print(i)
+
+contar_ate_vinte()
+
+def conte_usuario():
+    contagem_usuario = int(input("Digite um número para poder fazer a contagem: "))
+
+    for i in range(1, contagem_usuario):
+        print(i)
+conte_usuario()
 
 def tabuada_2():
 
@@ -21,3 +21,13 @@ def tabuada_2():
         i += 1
 tabuada_2()
 
+def tabuada_usuario():
+    i = 1
+    digito = int(input("Digite um número inteiro para imprimir a tabuada: "))
+    print("A tabuada do número é:")
+    while i <= 10:
+        soma = digito * i
+        print(f"{digito} * {i} = {soma}")
+        i += 1
+
+tabuada_usuario()
