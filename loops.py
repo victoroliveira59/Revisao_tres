0c8f68d64c1c37bd76137776cb8e3f8133f11043
victor samuel oliveira <victorsamueloliveira59@gmail.com>
2024-04-04 21:31:47 -0300

    Resolve questão 07

--- loops.py
+++ loops.py
@@ -1,14 +1,23 @@
-def contar_ate_vinte():
-    for i in range(20):
-        print(i)
+# def contar_ate_vinte():
+#     for i in range(20):
+#         print(i)
+#
+# contar_ate_vinte()
+#
+# def conte_usuario():
+#     contagem_usuario = int(input("Digite um número para poder fazer a contagem: "))
+#
+#     for i in range(1, contagem_usuario):
+#         print(i)
+# conte_usuario()
 
-contar_ate_vinte()
+def tabuada_2():
 
-def conte_usuario():
-    contagem_usuario = int(input("Digite um número para poder fazer a contagem: "))
-
-    for i in range(1, contagem_usuario):
-        print(i)
-conte_usuario()
-
+    i = 1
+    print(f"Tabuada de: 2")
+    while i <= 10:
+        soma = 2 + i
+        print(f"{2} + {i} = {soma}")
+        i += 1
+tabuada_2()
 
