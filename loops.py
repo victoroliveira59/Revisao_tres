4d0f022212bebbecfa77b1a4465cfacb064635f4
victor samuel oliveira <victorsamueloliveira59@gmail.com>
2024-04-04 21:27:35 -0300

    Resolve questão 06

--- loops.py
+++ loops.py
@@ -4,5 +4,11 @@ def contar_ate_vinte():
 
 contar_ate_vinte()
 
+def conte_usuario():
+    contagem_usuario = int(input("Digite um número para poder fazer a contagem: "))
+
+    for i in range(1, contagem_usuario):
+        print(i)
+conte_usuario()
 
 
